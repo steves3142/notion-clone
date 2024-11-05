fade0be236e4a664657f6bcf5ad16dcd564c5722
steves3142 <helixdeveloper1@gmail.com>
2024-11-04 23:23:30 -0500

    live cursor fully working

---
+++ notion-app/app/auth-endpoint/route.ts
@@ -0,0 +1,40 @@
+import { adminDb } from "@/firebase-admin";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import liveblocks from "@/lib/liveblocks";
+
+export async function POST(req: NextRequest) {
+  auth(); // Ensure the user is authenticated
+
+  const { userId, sessionClaims } = await auth();
+  const { room } = await req.json();
+
+  const session = liveblocks.prepareSession(sessionClaims?.email!, {
+    userInfo: {
+      name: sessionClaims?.fullName!,
+      email: sessionClaims?.email!,
+      avatar: sessionClaims?.image!,
+    },
+  });
+
+  const usersInRoom = await adminDb
+    .collectionGroup("rooms")
+    .where("userId", "==", sessionClaims?.email)
+    .get();
+
+  const userInRoom = usersInRoom.docs.find((doc) => doc.id === room);
+
+  if (userInRoom?.exists) {
+    session.allow(room, session.FULL_ACCESS);
+    const { body, status } = await session.authorize();
+
+    console.log("You are authorised");
+
+    return new Response(body, { status });
+  } else {
+    return NextResponse.json(
+      { message: "You are not in this room" },
+      { status: 403 }
+    );
+  }
+}
