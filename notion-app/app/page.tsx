e1b7ebae7256fcc2b8c3d268607dd4a0d2c56dd6
steves3142 <helixdeveloper1@gmail.com>
2024-11-28 20:40:37 -0500

    successfully deployed to production

--- notion-app/app/page.tsx
+++ notion-app/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex space-x-2 items-center animate-pulse">
       <ArrowLeftCircle className="w-12 h-12" />
-      <h1 className="font-bold">Get started with creating a New Document</h1>
+      <h1 className="font-bold">Get started with creating a New Document BUT sign in first</h1>
     </main>
   );
 }
